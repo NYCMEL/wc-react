6878e5b7e6f587b532ccd3f822d2f92d2f62d0f0
Mel on MacbookPro <mel@melify.com>
2016-03-13 09:38:07 -0400

    committed

---
+++ mtk/dev/tk/src/utl/tk.form.upload.tcl
@@ -0,0 +1,87 @@
+###HEADS###################################################################
+#
+#   COPYRIGHT (C) 1993,2009 Melify                                       
+#   EMAIL:        Support@Melify.com                                        
+#   URL:          http://www.Melify.com                                    
+#   
+#   A LICENSE IS REQUIRED FOR ALL PUBLIC FACING USE OF THIS TOOLKIT
+#   
+#   SOME JAVASCRIPTS INCLUDED IN THIS PACKAGE MAY REQUIRE A SEPARATE         
+#   LICENSE, PLEASE COMPLY WITH THEIR LICENSING AGREEMENTS                   
+#
+###HEADE###################################################################
+
+namespace eval tk {
+    namespace eval form {
+	namespace eval upload {}
+    }
+}
+
+######################################################
+##### 
+######################################################
+m::proc -public tk::form::upload::init {
+    -name:required
+    {-id         {}}
+    {-callback   {}}
+    -guts:required
+} {
+    DOCUMENTATION GOES HERE...
+} {
+    Trace
+    
+    set id [expr {($id == "") ? "$name" : "$id"}]
+    
+    cgi_form "[URL]" enctype=multipart/form-data method="POST" name="$name" id=$id autocomplete="off" {
+	export ajax=1
+	export callback=$callback
+	
+	uplevel $guts
+    }
+}
+
+######################################################
+##### 
+######################################################
+m::proc -public tk::form::upload::cb {
+} {
+    Documentation goes here...
+} {    
+    Trace
+
+    set cgi  [lindex $::file 0]
+    set name [file tail [lindex $::file 1]]
+    regsub -all {\[} $name "" name
+    regsub -all {\]} $name "" name
+
+    set r [file:read $cgi]
+
+    division class="alert alert-info" {
+	p [style padding 20px border "1px brown solid" border-radius 5px] $r
+    }
+
+    # javascript {
+    # 	put {
+    # 	    setTimeout(function(){
+    # 		document.location.href="/mtk/render?page=alan"
+    # 	    }, 8000);
+    # 	}
+    # }
+}
+
+######################################################
+##### 
+######################################################
+m::proc -public tk::form::upload::test {
+} {
+    DOCUMENTATION GOES HERE...
+} {
+    Trace
+    
+    tk::form::upload::init -name "test" -callback tk::form::upload::cb -guts {
+	file_button file class="btn btn-default"
+	br
+	submit_button action=[list Upload this file] class="btn btn-default"
+    }
+}
+
