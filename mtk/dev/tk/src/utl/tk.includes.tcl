2720e2ab935d3e222b484ff1a84d6b2a8f4caaa7
Mel on MacbookPro <mel@melify.com>
2016-03-14 14:26:09 -0400

    committed

--- mtk/dev/tk/src/utl/tk.includes.tcl
+++ mtk/dev/tk/src/utl/tk.includes.tcl
@@ -126,7 +126,7 @@ m::proc -private tk::include::viewport {
     put {
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=no">
+	<meta name=viewport content="width=device-width, initial-scale=1">
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	
 	<meta name="description" content="applications at the speed of thought">
