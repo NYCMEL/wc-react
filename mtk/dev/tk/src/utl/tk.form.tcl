9bbe8c0f04f495da0ce4b7b1943856987a3f906e
MacbookPro <mel.heravi@gmail.com>
2016-05-21 17:15:37 -0400

    committed

--- mtk/dev/tk/src/utl/tk.form.tcl
+++ mtk/dev/tk/src/utl/tk.form.tcl
@@ -21,7 +21,7 @@ namespace eval tk {
     namespace eval form {
 	array set pattern {
 	    text	{[a-zA-Z ]+}
-	    alnum	{[a-zA-Z0-9 ]+}
+	    alnum	{[a-zA-Z0-9\s]+}
 	    phone	{^(?d{3})?[- ]?d{3}[- ]?d{4}$}
 	    email	{[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$}
 	    city	{[A-Za-z ]+}
