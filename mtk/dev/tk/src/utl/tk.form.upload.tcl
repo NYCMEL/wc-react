b7c421b8552b3f3cb64e116e711677bd6b23f8c4
MacbookPro <mel.heravi@gmail.com>
2016-05-22 09:17:28 -0400

    committed

--- mtk/dev/tk/src/utl/tk.form.upload.tcl
+++ mtk/dev/tk/src/utl/tk.form.upload.tcl
@@ -71,39 +71,9 @@ m::proc -public tk::form::upload::test {
     Trace
     
     tk::form::upload::init -name "test" -callback tk::form::upload::cb -guts {
-	if {0} {
-	    file_button file class="btn btn-default custom-file-upload"
-	    br
-	    submit_button action=[list Upload this file] class="btn btn-default"
-	} else {
-	    put {
-		<label for="file-upload" class="custom-file-upload">
-		<i class="fa fa-cloud-upload"></i> Upload File
-		</label>
-
-		<input id="file-upload" type="file" />
-	    }
-	}
-    }
-
-    javascript {
-	put {
-	    $(document).ready(function() {
-		$('input[type=file]').each(function(){
-		    $(this).addClass('file').addClass('hidden');
-		    $(this).parent().append($('<div class="fakefile" />').append
-					    ($('<input type="text" class="form-control"/>')
-					     .attr('id',$(this).attr('id')+'__fake')).append($('<img src="pix/button_select.gif" alt="" />')));
-		    
-		    $(this).bind('change', function() {
-			$('#'+$(this).attr('id')+'__fake').val($(this).val());;
-		    });
-		    $(this).bind('mouseout', function() {
-			$('#'+$(this).attr('id')+'__fake').val($(this).val());;
-		    });
-		});
-	    });
-	}
+	file_button file class="btn btn-default"
+	br
+	put "<button type='submit' class='btn btn-default'><i class='fa fa-cloud-upload'></i> Upload File</button>"
     }
 }
 
