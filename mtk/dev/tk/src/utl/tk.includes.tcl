251c7e11aaba5d7871f9851f24e96ea5659ca0c6
MacbookPro <mel.heravi@gmail.com>
2016-05-29 16:59:03 -0400

    committed

--- mtk/dev/tk/src/utl/tk.includes.tcl
+++ mtk/dev/tk/src/utl/tk.includes.tcl
@@ -21,6 +21,81 @@ namespace eval tk {
     namespace eval include {}
 }
 
+if {[info exist require] == 0} {
+    set require ""
+}
+
+switch $require {
+    "include" {
+	put "<script src='/tk/inc/include.js'></script>"
+    }
+    "yepnope" {
+	put "<script src='/GitHub/yepnope.js/yepnope.1.5.4-min.js'></script>"
+    }
+}
+
+######################################################
+##### 
+######################################################
+proc include {fname} {
+    Trace
+
+    # DO NOT ALLOW DUPLICATES
+    set t $fname
+
+    if {[info exist ::_jscss_($t)] != 0} {
+ 	return
+    }
+
+    set ::_jscss_($t) 1
+    
+    if {[string range $fname 0 3] == "http"} {
+	set url "$fname"
+    } else {
+	set url "http://$::env(HTTP_HOST)$fname"
+    }
+
+    switch [file extension $t] {
+	".js" - ".json" {
+	    set func [file tail $fname]
+	    regsub -all {\-} $func "" func
+	    regsub -all {\_} $func "" func
+	    regsub -all {\.} $func "" func
+
+	    switch $::require {
+		"include" {
+		    put "<script>loadjscssfile('$url','js')</script>"
+		}
+		"yepnope" {
+		    put "<script>yepnope('$url')</script>"
+		}
+		default {
+		    put "<script type='text/javascript' media='all' src='$url'></script>"
+		}
+	    }
+	}
+	".css" {
+	    switch $::require {
+		"include" {
+		    put "<script>loadjscssfile('$url','css')</script>"
+		}
+		"yepnope" {
+		    put "<script>yepnope('$url')</script>"
+		}
+		default {
+		    put "<link type='text/css' href='$url' media='all' rel='stylesheet' />\n"
+		}
+	    }
+	}
+	".html" {
+	    put [file:read $fname]
+	}
+	".tcl" {
+	    source $fname
+	}
+    }
+}
+
 ##################################################
 ##### 
 ##################################################
