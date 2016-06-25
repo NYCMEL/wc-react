cf101a40a105fad1db2172dca9762d0f36e2da5b
NYCMEL <mel@melify.com>
2016-06-25 11:31:35 -0400

    committed

--- bin/mail.py
+++ bin/mail.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python
 
-import smtplib
+import sys
+import os
+import re
 
-server = smtplib.SMTP_SSL('smtpout.secureserver.net', 465)
-#server.starttls()
+from smtplib import SMTP_SSL as SMTP       # this invokes the secure SMTP protocol (port 465, uses SSL)
+# from smtplib import SMTP                  # use this for standard SMTP protocol   (port 25, no encryption)
 
-server.login("info@melify.com", "Melify")
+# old version
+# from email.MIMEText import MIMEText
+from email.mime.text import MIMEText
 
-msg = "Please work!!!!!!"
-fromaddr = "info@melify.com"
-toaddr = "mel.heravi@gmail.com"
-toaddr = "melify@yahoo.com"
+sender =     "mel.heravi@gmail.com"
+destination = ["MelifyIT@gmail.com"]
 
-server.sendmail(fromaddr, toaddr, msg)
+USERNAME = "info@melify.com"
+PASSWORD = "Melify"
 
+# typical values for text_subtype are plain, html, xml
+text_subtype = "plain"
+content      = """Test message"""
+subject      = "Sent from Python"
+
+try:
+    msg           = MIMEText(content, text_subtype)
+    msg["Subject"]= subject
+    msg["From"]   = sender # some SMTP servers will do this automatically, not all
+    conn          = SMTP("smtpout.secureserver.net")
+
+    conn.set_debuglevel(True)
+    conn.login(USERNAME, PASSWORD)
+
+    try:
+        conn.sendmail(sender, destination, msg.as_string())
+    finally:
+        conn.quit()
+
+except Exception, exc:
+    sys.exit( "mail failed; %s" % str(exc) ) # give a error message
