8d3bc32df824f1ce37088be2189726f72dbfd758
MacbookPro <mel.heravi@gmail.com>
2016-06-25 17:40:38 -0400

    committed

--- bin/mail/mail.tcl
+++ bin/mail/mail.tcl
@@ -2,13 +2,14 @@
 
 package require smtp
 package require mime
-set mailfrom mel.heravi@gmail.com
-set mailto mel.heravi@gmail.com
+
+set mailfrom	mel.heravi@gmail.com
+set mailto	mel.heravi@gmail.com
 set mailsubject HELLO
-set body "TESTING"
-set myHost smtpout.secureserver.net
-set myPort 456
-set myUname Info@TurnOnGreen.com
+set body	TESTING
+set myHost	smtpout.secureserver.net
+set myPort	456
+set myUname	Info@TurnOnGreen.com
 set myUpassword RedOlive1
 
 set token [mime::initialize -canonical "text/plain" -encoding "7bit" -string $body]
