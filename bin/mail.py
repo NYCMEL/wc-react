63d351233e388b221e9cdfd3d76743d52c587d53
NYCMEL <mel@melify.com>
2016-06-25 11:12:16 -0400

    committed

--- bin/mail.py
+++ bin/mail.py
@@ -3,12 +3,14 @@
 import smtplib
 
 server = smtplib.SMTP_SSL('smtpout.secureserver.net', 465)
+#server.starttls()
 
 server.login("info@melify.com", "Melify")
 
 msg = "Please work!!!!!!"
 fromaddr = "info@melify.com"
 toaddr = "mel.heravi@gmail.com"
+toaddr = "melify@yahoo.com"
 
 server.sendmail(fromaddr, toaddr, msg)
 
