c58aa55a0011729edd45f2c087dd2eb0c62f263c
MacbookPro <mel.heravi@gmail.com>
2016-09-13 16:18:32 -0400

    committed

--- mtk/dev/tk/src/utl/util.tcl
+++ mtk/dev/tk/src/utl/util.tcl
@@ -691,7 +691,7 @@ m::proc -public tk::char {
 ######################################################
 m::proc -public tk::pretty {
     {-type "lang-html"}
-    -text:required
+    -str:required
 } {
     Documentation goes here...
 } {  
@@ -699,7 +699,7 @@ m::proc -public tk::pretty {
     tk::include::prettify
  
     preformatted class="prettyprint prettify $type linenums" {
-	uplevel $text
+	uplevel $str
     }
 }
 
