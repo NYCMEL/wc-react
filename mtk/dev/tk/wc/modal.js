59e9304052ae9aec9dd92428130f69ac13ad8289
MacbookPro <mel.heravi@gmail.com>
2016-10-05 13:11:05 -0400

    committed

--- mtk/dev/tk/wc/modal.js
+++ mtk/dev/tk/wc/modal.js
@@ -1,3 +1,13 @@
+var prefix = 'bs';
+var importDoc = document.currentScript === document._currentScript ? document.currentScript.ownerDocument: document._currentScript.ownerDocument;
+
+// Create <style> elm with template CSS
+var css = importDoc.querySelector('#x-css');
+var style = document.createElement('style');
+
+style.appendChild(css.content.cloneNode(true));
+document.head.appendChild(style);
+
 // Modal
 XModal = document.registerElement(prefix + '-modal', {
     prototype: Object.create(HTMLElement.prototype, {
