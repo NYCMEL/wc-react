e4ad75536f8e8b6e0222878eec7244c134474fce
MacbookPro <mel.heravi@gmail.com>
2016-10-18 20:30:01 -0400

    committed

--- mtk/dev/tk/wc/inc/accordion.js
+++ mtk/dev/tk/wc/inc/accordion.js
@@ -18,76 +18,10 @@ Accordion.createdCallback = function() {
 
     var content = host.innerHTML;
 
+    alert(id);
+
     console.groupEnd();
 };
-
-////////////////////////////////////////////////////////////////////////////////////
-//// 
-////////////////////////////////////////////////////////////////////////////////////
-Accordion.toggle = function(id) {
-    var handle = this;
-    var shadow = this.shadowRoot;
-
-    console.group("Accordion.toggle");
-    
-    var fa = shadow.querySelector(".accordion-heading .fa");
-    
-    if (fa.classList.contains("fa-angle-down")) {
-	handle.close(id);
-    } else {
-	handle.open(id);
-    }
-    
-    console.groupEnd();
-};
-
-////////////////////////////////////////////////////////////////////////////////////
-//// 
-////////////////////////////////////////////////////////////////////////////////////
-Accordion.open = function(id) {
-    console.group("Accordion.open", id);
-
-    if (typeof id === "undefined") {
-	id = this.getAttribute("id");
-    }
-
-    console.log('BROADCASTING accordionOpened', id);
-    radio('accordionOpened').broadcast(id);
-    
-    var shadow = this.shadowRoot;
-    var fa = shadow.querySelector(".accordion-heading .fa");
-    
-    fa.classList.remove("fa-angle-right");
-    fa.classList.add("fa-angle-down");
-    
-    shadow.querySelector(".accordion-body").setAttribute("style", "display:block");
-
-    console.groupEnd();
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-//// 
-////////////////////////////////////////////////////////////////////////////////////
-Accordion.close = function(id) {
-    console.group("Accordion.close", id);
-    
-    if (typeof id === "undefined") {
-	id = this.getAttribute("id");
-    }
-
-    console.log('BROADCASTING accordionClosed', id);
-    radio('accordionClosed').broadcast(id);
-    
-    var shadow = this.shadowRoot;
-    var fa = shadow.querySelector(".accordion-heading .fa");
-
-    fa.classList.remove("fa-angle-down");
-    fa.classList.add("fa-angle-right");
-    
-    shadow.querySelector(".accordion-body").setAttribute("style", "display:none");
-
-    console.groupEnd();
-}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //// 
