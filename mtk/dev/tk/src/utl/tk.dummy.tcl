9f5b6cab0fae2528522120749c57de693cfdb817
MacbookPro <mel.heravi@gmail.com>
2016-10-19 12:35:22 -0400

    committed

--- mtk/dev/tk/src/utl/tk.dummy.tcl
+++ mtk/dev/tk/src/utl/tk.dummy.tcl
@@ -110,12 +110,10 @@ m::proc -public tk::dummy::3 {
 
     set x [clock format [clock seconds] -format "%T"]
 
-    division style=overflow:auto\;height:100% {
-	for {set i 0} {$i <= $size} {incr i} {
-	    division style=padding-bottom:10px\;padding:10px {
-		put "[string toupper [lorem -language $language 4]] - $x";br
-		put [lorem -language $language 30]
-	    }
+    for {set i 0} {$i <= $size} {incr i} {
+	division style=padding-bottom:10px\;padding:10px {
+	    put "[string toupper [lorem -language $language 4]] - $x";br
+	    put [lorem -language $language 30]
 	}
     }
 }
