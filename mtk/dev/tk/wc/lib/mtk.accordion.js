0e590612e2df7e671f4c32d008266a94cc715c81
MacbookPro <mel.heravi@gmail.com>
2016-10-19 09:47:45 -0400

    committed

--- mtk/dev/tk/wc/lib/mtk.accordion.js
+++ mtk/dev/tk/wc/lib/mtk.accordion.js
@@ -17,6 +17,14 @@ Accordion.createdCallback = function() {
     var content = host.innerHTML;
 
     host.innerHTML = "<style>@import '/tk/wc/lib/mtk.css';</style>" + content;
+    xx = host;
+
+    let panels = host.querySelectorAll("mtk-panel")
+
+    for (i=0; i< panels.length; i++) {
+        console.log(">>>", i, panels[i]);
+    }
+
 
     console.groupEnd();
 };
