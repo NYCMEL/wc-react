7215c8537484055ba311235faaf6c92dfd36c504
MacbookPro <mel.heravi@gmail.com>
2016-10-19 09:52:58 -0400

    committed

--- mtk/dev/tk/wc/lib/mtk.accordion.js
+++ mtk/dev/tk/wc/lib/mtk.accordion.js
@@ -13,20 +13,18 @@ Accordion.createdCallback = function() {
     }
     
     var id = this.getAttribute("id");
-    var show = this.getAttribute("show");
+    var show = this.getAttribute("show") || 0;
     var host = document.querySelector("#" + id);
     var content = host.innerHTML;
 
     host.innerHTML = "<style>@import '/tk/wc/lib/mtk.css';</style>" + content;
-    xx = host;
 
     let panels = host.querySelectorAll("mtk-panel")
 
     for (i=0; i< panels.length; i++) {
 	let pid = panels[i].getAttribute("id");
-        console.log(">>>", show, pid, i, panels[i]);
 
-	if (show != pid) {
+	if (show != i) {
 	    document.querySelector("#" + pid).close();
 	}
     }
