3a04a73267f6424f8d0d22d1e04288453618b5a2
MacbookPro <mel.heravi@gmail.com>
2016-11-24 17:51:58 -0500

    committed

--- mtk/dev/tk/src/utl/tk.includes.tcl
+++ mtk/dev/tk/src/utl/tk.includes.tcl
@@ -272,6 +272,7 @@ m::proc -private tk::include::bootstrap:4 {
     # BOOTSTRAP 4
     include "/GitHub/tether/dist/css/tether.min.css"
     include "/GitHub/bootstrap/dist/css/bootstrap.min.css"
+    include "/GitHub/bootstrap/dist/css/bootstrap-flex.min.css"
     
     include "/GitHub/tether/dist/js/tether.min.js"
     include "/GitHub/bootstrap/dist/js/bootstrap.min.js"
