9838c0e9f5e0cd1382c0b7f19ad75ca5974df6bc
MacbookPro <mel.heravi@gmail.com>
2017-01-03 11:13:55 -0500

    committed

--- bin/cron/watch.sass.sh
+++ bin/cron/watch.sass.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-cd /Melify && /usr/local/bin/sass --watch --style compressed .
+cd /Melify/mtk/dev && /usr/local/bin/sass --watch --style compressed .
