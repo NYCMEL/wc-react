01143be6ea38a355b665913457b270142a436826
MacbookPro <mel.heravi@gmail.com>
2017-01-03 11:16:53 -0500

    committed

--- bin/cron/watch.less.sh
+++ bin/cron/watch.less.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-cd /Melify && /usr/local/bin/watch-less
+# cd /Melify && /usr/local/bin/watch-less
+
+MYPROG="watch-less"
+RESTART="watch-less"
+PGREP="/usr/bin/pgrep"
+
+# FIND MYPROG PID
+$PGREP ${MYPROG}
+
+# IF NOT RUNNING
+if [ $? -ne 0 ]
+then
+   $RESTART
+fi
