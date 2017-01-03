ce703788db1e453722ad49de304f58c32a5bfc18
MacbookPro <mel.heravi@gmail.com>
2017-01-03 10:59:28 -0500

    committed

--- bin/cron/watch.less.sh
+++ bin/cron/watch.less.sh
@@ -2,4 +2,3 @@
 PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 cd /Melify && /usr/local/bin/watch-less
-cd /Melify && /usr/local/bin/sass --watch --style compressed .
---
+++ bin/cron/watch.sass.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+cd /Melify && /usr/local/bin/sass --watch --style compressed .
