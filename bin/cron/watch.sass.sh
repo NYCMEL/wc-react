73e5754e60129410f83a5beb238ebf3f5dfbaef8
MacbookPro <mel.heravi@gmail.com>
2017-01-03 11:18:17 -0500

    committed

--- bin/cron/watch.sass.sh
+++ bin/cron/watch.sass.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-cd /Melify/mtk/dev && /usr/local/bin/sass --watch --style compressed .
+# cd /Melify/mtk/dev && sass --watch --style compressed .
+
+MYPROG="sass"
+RESTART="sass --watch --style compressed ."
+PGREP="/usr/bin/pgrep"
+
+# FIND MYPROG PID
+$PGREP ${MYPROG}
+
+# IF NOT RUNNING
+if [ $? -ne 0 ]
+then
+   $RESTART
+fi
