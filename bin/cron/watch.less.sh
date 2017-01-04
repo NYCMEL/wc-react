b101c28c6f42b19da92209b22c2b94de841b5322
MacbookPro <mel.heravi@gmail.com>
2017-01-04 11:45:42 -0500

    committed

--- bin/cron/watch.less.sh
+++ bin/cron/watch.less.sh
@@ -5,7 +5,7 @@ PATH=$PATH:/usr/local/bin
 cd /Melify/mtk/dev
 
 MYPROG="watch.less.sh"
-RESTART="/usr/local/bin/watch-less -e css  >> /tmp/cron.less.log"
+RESTART="/usr/local/bin/watch-less -e css"
 PGREP="/usr/bin/pgrep"
 
 # FIND MYPROG PID
