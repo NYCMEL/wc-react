c7b7aaab3b54050ed24fb0202f1f46baaee9bdf1
MacbookPro <mel.heravi@gmail.com>
2017-01-24 10:51:03 -0500

    committed

---
+++ mtk/dev/tk/src/utl/form/tk.calendar.tcl
@@ -0,0 +1,77 @@
+###HEADS###################################################################
+#
+#  Melify Internet Toolkit (MTK) - Copyright (C) 2015  Melify LLC.
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses.
+#
+###HEADE###################################################################
+
+namespace eval tk {}
+
+include "/GitHub/bootstrap-datepicker2/dist/css/bootstrap-datepicker3.standalone.min.css"
+include "/GitHub/bootstrap-datepicker2/dist/js/bootstrap-datepicker.min.js"
+
+######################################################
+##### 
+######################################################
+m::proc -public tk::calendar {
+    -id:required
+    -name:required
+    {-label ""}
+    -options:required
+    {-selected ""}
+    {-help  ""}
+    args
+} {
+    Documentation goes here...
+} {    
+    Trace
+
+    division class="form-group" {
+	if {$label != ""} {
+	    label id="$id-label" for="$id-label" "$label"
+	}
+	
+	put {
+<div class="input-group date" data-provide="datepicker">
+    <input type="text" class="form-control">
+    <div class="input-group-addon">
+        <span class="glyphicon glyphicon-th"></span>
+    </div>
+</div>
+	}
+	
+	if {$help != ""} {
+	    put "<small id='$id-help' class='form-text text-muted'>$help</small>"
+	}
+    }    
+}
+
+######################################################
+##### TEST
+######################################################
+m::proc -public tk::calendar:test {
+} {
+    Documentation goes here...
+} {    
+    Trace
+
+    tk::calendar -id "my-select" -name "v(select)" -options [subst {
+	"" "PLEASE SELECT"
+	1 "Option 1"
+	2 "Option 2"
+	3 "Option 3"
+	4 "Option 4"
+    }] -selected 2 -help "help text goes here..." required
+}
