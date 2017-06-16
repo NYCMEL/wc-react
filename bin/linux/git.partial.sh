2c4cdcd47b0fc66f5ab9d814f43e7fa2c7b511d7
Mel <mel.heravi@gmail.com>
2017-06-16 09:01:41 -0400

    committed

--- bin/linux/git.partial.sh
+++ bin/linux/git.partial.sh
@@ -2,21 +2,50 @@
 
 usage ()
 {
-  echo 'Usage : git.partial <path-to-git-repo> <partial-folder-name>'
+  echo 'Usage : git.partial -path <path-to-git-repo> -folder <partial-folder-name>'
   exit
 }
 
-if [ "$#" -ne 2 ]
+# CHECK ARGUMENT COUNT
+if [ "$#" -ne 4 ]
 then
   usage
 fi
 
+# ASSIGN VARIABLES FROM OPTIONS
+while [ "$1" != "" ]; do
+case $1 in
+        -path )        shift
+                       REPO=$1
+                       ;;
+        -folder )      shift
+                       FOLDER=$1
+                       ;;
+    esac
+    shift
+done
+
+while [ "$1" != "" ]; do
+case $1 in
+        -path   ) shift
+                  REPO=$1
+                  ;;
+
+        -folder ) shift
+                  FOLDER=$1
+                  ;;
+esac
+    shift
+done
+
+# EXECUTE REQUIRED COMMANDS
 cd /tmp
 rm -rf test
 
 git init test
 cd test
-git remote add origin $1
+git remote add origin $REPO
 git config core.sparsecheckout true
-echo "$2/*" >> .git/info/sparse-checkout
+echo "$FOLDER/*" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
+
