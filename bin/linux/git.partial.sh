8a3018c1888f0d9b55c5ac2d49eeda40a20e08d3
Mel <mel.heravi@gmail.com>
2017-06-16 08:53:41 -0400

    committed

--- bin/linux/git.partial.sh
+++ bin/linux/git.partial.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# USE IT LIKE:
-#    ./git.partial https://github.com/NYCMEl/Melify-private.git emacs/*
+usage ()
+{
+  echo 'Usage : git.partial <path-to-git-repo> <partial-folder-name>'
+  exit
+}
 
-#!/bin/sh
+if [ "$#" -ne 2 ]
+then
+  usage
+fi
 
 cd /tmp
 rm -rf test
@@ -12,5 +18,5 @@ git init test
 cd test
 git remote add origin $1
 git config core.sparsecheckout true
-echo "$2" >> .git/info/sparse-checkout
+echo "$2/*" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
