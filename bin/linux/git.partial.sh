ce6f43402bf60d7dededa69010f6ccd396aeed02
Mel <mel.heravi@gmail.com>
2017-06-16 08:47:32 -0400

    committed

--- bin/linux/git.partial.sh
+++ bin/linux/git.partial.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 # USE IT LIKE:
-#    ./git.partial "jq/*"
+#    ./git.partial https://github.com/NYCMEl/Melify-private.git emacs/*
+
+#!/bin/sh
 
 cd /tmp
 rm -rf test
 
 git init test
 cd test
-git remote add origin https://github.com/NYCMEl/tk-components.git
+git remote add origin $1
 git config core.sparsecheckout true
-echo "$1" >> .git/info/sparse-checkout
+echo "$2" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
