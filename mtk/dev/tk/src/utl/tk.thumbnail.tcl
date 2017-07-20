327a6faa6cd1e874f50acacd896a554aeb5f08a2
Mel <mel.heravi@gmail.com>
2017-07-20 11:54:02 -0400

    committed

---
+++ mtk/dev/tk/src/utl/tk.thumbnail.tcl
@@ -0,0 +1,35 @@
+###HEADS###################################################################
+#
+#  Melify Internet Toolkit (MTK) - Copyright (C) 2015  Melify LLC.
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses.
+#
+###HEADE###################################################################
+
+namespace eval tk {}
+
+######################################################
+##### 
+######################################################
+m::proc -public tk::thumbnail {
+    -url:required
+    -name:required
+} {
+    Documentation goes here...
+} {    
+    Trace
+
+    put [url [img "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" data-url="$url"] $name]
+}    
+
