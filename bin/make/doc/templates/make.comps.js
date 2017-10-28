b424c5eb240fb3fec6c5f923727963e1bd1a09d9
Mel <mel@melify.com>
2017-10-28 11:50:34 -0400

    committed

--- bin/make/doc/templates/make.comps.js
+++ bin/make/doc/templates/make.comps.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////////
-//// Time-stamp: <2017-10-28 11:47:48 (melify)>
+//// Time-stamp: <2017-10-28 11:50:14 (melify)>
 /////////////////////////////////////////////////////////////////////////////////
 var comps = {};
 
@@ -13,12 +13,11 @@ comps.card = function(obj) {
 	<div class="card" style="width: 20rem;">
 	  <img class="card-img-top" src="CARD_IMG">
 	  <div class="card-block">
-	    <h4 class="card-title">CARD_TITLE</h4>
-	    <p class="card-text">CARD_DESCRIPTION</p>
-	    <a href="#" class="btn btn-primary">Go somewhere</a>
+	    <h4 class="card-title">Card Title</h4>
+	    <p class="card-description">Card Description</p>
 	  </div>
 	</div>`
-
+    
     let img   = obj.find("card-img").attr("src");
     let title = obj.find("card-title").html();
     let descr = obj.find("card-description").html();
