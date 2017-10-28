0ea11db481c4f566cb316e83b363b44b4f16a31a
Mel <mel@melify.com>
2017-10-28 15:02:11 -0400

    committed

--- bin/make/doc/templates/make.comps.js
+++ bin/make/doc/templates/make.comps.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////////
-//// Time-stamp: <2017-10-28 14:50:49 (melify)>
+//// Time-stamp: <2017-10-28 15:01:59 (melify)>
 /////////////////////////////////////////////////////////////////////////////////
 var comps = {};
 
@@ -9,23 +9,27 @@ var comps = {};
 comps.card = function(obj) {
     console.group("comps.card:", obj);
 
-    let img     = obj.find("card-img").attr("src");
-    let iheight = obj.find("card-img").attr("height") || "100%"
-    let title   = obj.find("card-title").html();
-    let body   = obj.find("card-body").html();
+    obj.css("display","inline-block");
+
+    let cw    = obj.attr("width");
+    let ch    = obj.attr("heihgt");
+    let img   = obj.find("card-img").attr("src");
+    let imgh  = obj.find("card-img").attr("height") || "100%"
+    let title = obj.find("card-title").html();
+    let body  = obj.find("card-body").html();
 
     let html = `
-	<div class="card">
-	  <img class="card-img-top" src="CARD_IMG" style="height:${iheight}">
+	<div class="card" style="width:${cw}px;height:${ch}px;">
+	  <img class="card-img-top" src="CARD_IMG" style="height:${imgh}">
 	  <div class="card-block">
 	    <h4 class="card-title">CARD_TITLE</h4>
-	    <p class="card-body">CARD_BODY</p>
+	    <p class="card-body" style=height:${ch};overflow:auto;>CARD_BODY</p>
 	  </div>
 	</div>`
     
-    html = html.replace("CARD_IMG", img);
+    html = html.replace("CARD_IMG",     img);
     html = html.replace("CARD_TITLE", title);
-    html = html.replace("CARD_BODY", body);
+    html = html.replace("CARD_BODY",   body);
 
     obj.html(html);
 
