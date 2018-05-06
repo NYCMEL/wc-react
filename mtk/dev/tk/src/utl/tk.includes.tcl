e4f68c8d3e3ee3eb3faef3ae722d7b52cf92102f
Mel <mel@melify.com>
2018-05-06 09:57:04 -0400

    committed

--- mtk/dev/tk/src/utl/tk.includes.tcl
+++ mtk/dev/tk/src/utl/tk.includes.tcl
@@ -358,7 +358,7 @@ m::proc -private tk::include::common {
     }
 
     # THESE COME AHEAD OF THEM ALL
-    include "/GitHub/Font-Awesome/css/font-awesome.min.css"
+    include "/GitHub/font-awesome/web-fonts-with-css/css/fontawesome-all.css"
     include "/tk/inc/min/tk.common.min.css"
 
     include "$jqv"
