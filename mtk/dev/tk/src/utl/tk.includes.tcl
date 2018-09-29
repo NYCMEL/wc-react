c69f6774f6a5ce93977dde8c28c00f086ed4bcf6
Mel <melheravi@gmail.com>
2018-09-29 10:13:19 -0400

    committed

--- mtk/dev/tk/src/utl/tk.includes.tcl
+++ mtk/dev/tk/src/utl/tk.includes.tcl
@@ -21,7 +21,6 @@ namespace eval tk {
     namespace eval include {}
 }
 
-set bs  [expr {([info exist      bs] == "0") ? "3" : "4"}]
 set cdn [expr {([info exist     cdn] == "0") ? "0" : "$::cdn"}]
 
 ######################################################
@@ -238,30 +237,14 @@ m::proc -private tk::include::bootstrap {
 } {
     Trace
 
-    switch $::bs {
-	"3" {
-	    include "/tk/inc/bootstrap.min.css"
-	    include "/tk/inc/bootstrap.min.js"
+    switch $::cdn {
+	"0" {
+	    include "/GitHub/bootstrap/dist/css/bootstrap.min.css"
+	    include "/GitHub/bootstrap/dist/js/bootstrap.bundle.min.js"
 	}
-	"4" {
-	    # BOOTSTRAP 4
-	    switch $::cdn {
-		"0" {
-		    include "/GitHub/tether/dist/css/tether.min.css"
-		    include "/GitHub/bootstrap/dist/css/bootstrap.min.css"
-		    
-		    include "/tk/inc/popper.min.js"
-		    include "/GitHub/tether/dist/js/tether.min.js"
-		    include "/GitHub/bootstrap/dist/js/bootstrap.min.js"
-		}
-		"1" {
-		    put {
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-		    }
-		}
-	    }
+	"1" {
+	    include "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+	    include "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"
 	}
     }
 }
