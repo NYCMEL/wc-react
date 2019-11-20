080c5366050d19d1d35bc5b1b1ce9cdf76b5ea56
Mel <melheravi@gmail.com>
2019-11-20 08:05:20 -0500

    committed

--- bin/mac/t/queue.tcl
+++ bin/mac/t/queue.tcl
@@ -1,3 +1,5 @@
+#!/Melify/bin/mac/tclkit
+
 set x [list]
 set ticks 3
 
@@ -12,6 +14,22 @@ proc queue {val} {
     }
 }
 
+proc dir {} {
+    global x
+    global ticks
+
+    set s1 [lsort -increasing $x]
+    set s2 [lsort -decreasing $x]
+
+    if {$s1 == $x} {
+	puts UUUUUUUUUUUUU
+    }
+
+    if {$s2 == $x} {
+	puts DDDDDDDDDDDDDDD
+    }
+}
+
 queue 50
 queue 80
 queue 10
@@ -20,3 +38,5 @@ queue 65
 queue 75
 
 puts $x
+
+dir
