105b1635a120e265ea411a7e39bbbed75c746531
Mel <melheravi@gmail.com>
2019-12-24 08:50:34 -0500

    committed

--- mtk/dev/app/ib/ib.tcl
+++ mtk/dev/app/ib/ib.tcl
@@ -1,11 +1,16 @@
+set last ""
 set f [open ./data/data.json w]
 
 namespace eval ib {
     proc quote {every symbols} {
 	if {[catch {
 	    foreach i  [json::json2dict [exec curl -s -k -X GET "https://localhost:5000/v1/portal/iserver/marketdata/snapshot?conids=$symbols"]] {
-		puts -nonewline .;flush stdout
-		puts $::f $i;flush $::f
+		if {$i != $::last} {
+		    puts -nonewline .;flush stdout
+		    puts $::f $i;flush $::f
+		    set ::last "$i"
+		    puts "$i";flush stdout
+		}
 	    }
 	} e] != 0} {
 	    puts >>>>>>>>>>$e
