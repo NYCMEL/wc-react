415ccd066db19fd3f5d6e4a42f508e53240610a1
Mel <melheravi@gmail.com>
2020-02-25 14:57:48 -0500

    committed

--- src/components/Headline/MTKHeadline.stories.tsx
+++ src/components/Headline/MTKHeadline.stories.tsx
@@ -5,17 +5,8 @@ export default {title: 'MTK Headline'}
 
 export const basic = () => (
     <div id="wc">
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-md-12 aem">
-                    <MTKHeadline
-                        author={"Mel"}
-                        body={<div>body content</div>}
-                        date="2/25/2020"
-                        title={<h1>title</h1>}
-                    />
-                </div>
-            </div>
-        </div>
+	<div className="aem pt-5">
+            <MTKHeadline author={"Mel"} body={<div>body content</div>} date="2/25/2020" title={<h1>title</h1>} />
+	</div>
     </div>
 )
