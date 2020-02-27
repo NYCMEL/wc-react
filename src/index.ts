ea657a0ef4a9bda7d6e714e0753573738062a891
Mel <melheravi@gmail.com>
2020-02-27 11:23:51 -0500

    committed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {MTKTicker} from './components/Ticker/MTKTicker';
+export {MTKTicker}    from './components/Ticker/MTKTicker';
 export {MTKAccordion} from './components/Accordion/MTKAccordion';
-export {MTKFlip} from './components/Flip/MTKFlip';
+export {MTKFlip}      from './components/Flip/MTKFlip';
