23f000647b528def978a46f3b70713eddb34c90b
Mel <melheravi@gmail.com>
2020-02-27 10:43:56 -0500

    committed

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export {MTKTicker} from './components/Ticker/MTKTicker';
 export {MTKAccordion} from './components/Accordion/MTKAccordion';
 export {MTKFlip} from './components/Flip/MTKFlip';
 
+import "./index.css";
