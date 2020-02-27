d68c7d3af1a8fb9e35b3498a5cbf160b02941f57
Mel <melheravi@gmail.com>
2020-02-27 16:42:46 -0500

    committed

--- src/components/Banner/MTKBanner.stories.tsx
+++ src/components/Banner/MTKBanner.stories.tsx
@@ -11,6 +11,11 @@ export const basic = () => (
 		<div className="col-md-12">
 		    <MTKBanner title="MEL WAS HERE" subtitle={
 			<wc-include href="http://links.melify.com/mtk/render?callback=tk::dummy::3"></wc-include>
+		    } actions={
+			<div>
+			    <a id="btn-more-1" class="btn btn-primary btn-action mr-3" href="#" role="button">More I</a>
+			    <a id="btn-more-2" class="btn btn-primary btn-action" href="#" role="button">More II</a>
+			</div>
 		    }
 		    />
 		</div>
--- src/components/Banner/MTKBanner.tsx
+++ src/components/Banner/MTKBanner.tsx
@@ -21,6 +21,7 @@ interface MTKBannerProps extends CommonProps {
     id?: string;
     title: JSX.Element | string | JSX.Element[];
     subtitle: JSX.Element | string | JSX.Element[];
+    actions: JSX.Element | string | JSX.Element[];
 }
 
 export const MTKBanner = (props: MTKBannerProps) => {
@@ -38,24 +39,23 @@ export const MTKBanner = (props: MTKBannerProps) => {
     return (
         <div role='heading'>
 	    <wc-banner {...props}>
-		<wc-banner-lhs className="col-md-6">
+		<wc-banner-lhs class="col-md-6 d-block">
 		    <div>
-			<wc-banner-title className="display-4">
-			    <h2>{props.title}</h2>
+			<wc-banner-title class="display-4">
+			    {props.title}
 			</wc-banner-title>
 
-			<wc-banner-subtitle className="lead">
+			<wc-banner-subtitle class="lead">
 			    {props.subtitle}
 			</wc-banner-subtitle>
 			
-			<wc-banner-actions className="lead">
-			    <a id="btn-more-1" className="btn btn-primary btn-action" href="#" role="button">More I</a>
-			    <a id="btn-more-2" className="btn btn-primary btn-action" href="#" role="button">More II</a>
+			<wc-banner-actions class="lead">
+			    {props.actions}
 			</wc-banner-actions>
 		    </div>
 		</wc-banner-lhs>
 
-		<wc-banner-rhs className="col-md-6 cover"></wc-banner-rhs>
+		<wc-banner-rhs class="col-md-6 cover"></wc-banner-rhs>
 	    </wc-banner>
 	</div>
     )
