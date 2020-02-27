3e38eba01bdcfb04b36d6ee219723a0497d49351
Mel <melheravi@gmail.com>
2020-02-27 11:22:21 -0500

    committed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import './fonts/Roboto-Light.ttf';
-import './fonts/Roboto-Regular.ttf';
-import './fonts/Roboto-Medium.ttf';
-import './fonts/Roboto-Bold.ttf';
-
 export {MTKTicker} from './components/Ticker/MTKTicker';
 export {MTKAccordion} from './components/Accordion/MTKAccordion';
 export {MTKFlip} from './components/Flip/MTKFlip';
-
-import "./index.css";
