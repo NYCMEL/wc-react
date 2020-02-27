19029093b36ecaddb1304d9071fa7f09f6823185
Mel <melheravi@gmail.com>
2020-02-27 15:59:29 -0500

    committed

--- src/components/Banner/MTKBanner.tsx
+++ src/components/Banner/MTKBanner.tsx
@@ -35,32 +35,34 @@ export const MTKBanner = (props: MTKBannerProps) => {
         }
     }, []);
     return (
-	<wc-banner {...props}>
-	    <wc-banner-lhs className="col-md-6">
-		<div>
-		    <wc-banner-title className="display-4">
-			{props.title}
-		    </wc-banner-title>
+        <div role='heading'>
+	    <wc-banner {...props}>
+		<wc-banner-lhs className="col-md-6">
+		    <div>
+			<wc-banner-title className="display-4">
+			    {props.title}
+			</wc-banner-title>
 
-		    <wc-banner-subtitle className="lead">
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-			sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
-		    </wc-banner-subtitle>
+			<wc-banner-subtitle className="lead">
+			    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+			    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+			</wc-banner-subtitle>
 
-		    <p>
-			<hr/>
-			Hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilis
-		    </p>
+			<p>
+			    <hr/>
+			    Hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilis
+			</p>
 
-		    <wc-banner-actions className="lead">
-			<a id="btn-more-1" className="btn btn-primary btn-action" href="#" role="button">More I</a>
-			<a id="btn-more-2" className="btn btn-primary btn-action" href="#" role="button">More II</a>
-		    </wc-banner-actions>
-		</div>
-	    </wc-banner-lhs>
+			<wc-banner-actions className="lead">
+			    <a id="btn-more-1" className="btn btn-primary btn-action" href="#" role="button">More I</a>
+			    <a id="btn-more-2" className="btn btn-primary btn-action" href="#" role="button">More II</a>
+			</wc-banner-actions>
+		    </div>
+		</wc-banner-lhs>
 
-	    <wc-banner-rhs className="col-md-6 cover">
-	    </wc-banner-rhs>
-	</wc-banner>
+		<wc-banner-rhs className="col-md-6 cover">
+		</wc-banner-rhs>
+	    </wc-banner>
+	</div>
     )
 };
