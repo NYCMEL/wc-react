e352cd8994f723abb4ef7e030dfdbc82927d499e
Mel <mel@melify.com>
2020-02-27 18:14:06 -0500

    committed

--- src/components/Banner/MTKBanner.stories.tsx
+++ src/components/Banner/MTKBanner.stories.tsx
@@ -10,9 +10,10 @@ export const basic = () => (
 	    <div className="row">
 		<div className="col-md-12">
 		    <MTKBanner title="MEL WAS HERE" subtitle={
-			<wc-include href="http://links.melify.com/mtk/render?callback=tk::dummy::3"></wc-include>
+			<wc-include href="http://links.melify.com/mtk/render?callback=lorem&size=40"></wc-include>
 		    } actions={
 			<div>
+			    <hr/>
 			    <button className="btn btn-primary btn-action btn-more-1 mr-3" role="button">More I</button>
 			    <button className="btn btn-primary btn-action btn-more-2" role="button">Even More II</button>
 			</div>
