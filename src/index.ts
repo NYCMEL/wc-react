fce5583bf3281b6c07adf84c9f1ceeaba3dc7abf
Mel <melheravi@gmail.com>
2020-02-28 12:05:24 -0500

    committed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export {MTKTicker}    from './components/Ticker/MTKTicker';
 export {MTKAccordion} from './components/Accordion/MTKAccordion';
 export {MTKFlip}      from './components/Flip/MTKFlip';
+export {MTKBanner}    from './components/Banner/MTKBanner';
+export {MTKCard}      from './components/Card/MTKCard';
+export {MTKHeadline}  from './components/Headline/MTKHeadline';
+export {MTKapp}       from './components/app/MTKapp';
+export {MTKpager}     from './components/pager/MTKpager';
+export {MTKtabs}      from './components/tabs/MTKtabs';
+
