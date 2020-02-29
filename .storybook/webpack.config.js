a0eba079b6fca5a27ef9ab12d40323cc60ac598d
Mel <melheravi@gmail.com>
2020-02-28 20:22:42 -0500

    committed

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,26 +1,20 @@
 const path = require('path')
 const SRC_PATH = path.join(__dirname, '../src')
 
-module.exports = ({config}) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    include: [SRC_PATH],
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader'),
-        options: {
-          configFileName: './tsconfig.json'
-        }
-      }
-    ]
-  })
+module.exports = ({ config }) => {
+    config.module.rules.push({
+	test: /\.(ts|tsx)$/,
+	include: [SRC_PATH],
+	use: [
+	    {
+		loader: require.resolve('awesome-typescript-loader'),
+		options: {
+		    configFileName: './tsconfig.json'
+		}
+	    }
+	]
+    })
 
-  // config.module.rules.push({
-	// 	test: /\.css$/,
-	// 	loader: 'style-loader!css-loader',
-  //   include: path.resolve(__dirname, '../'),
-  // })
-
-  config.resolve.extensions.push('.ts', '.tsx')
-  return config
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
 }
