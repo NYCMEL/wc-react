62bfa7c4b21861d4a61556db240d758f89405997
Mel <melheravi@gmail.com>
2020-03-16 08:38:36 -0400

    committed

--- src/components/tabs/MTKtabs.stories.tsx
+++ src/components/tabs/MTKtabs.stories.tsx
@@ -9,7 +9,7 @@ export const basic = () => (
 	<div className="container mt-5">
 	    <div className="row">
 		<div className="col-md-6">
-		    <MTKtabs id="my-tabs" cfg="http://links.melify.com/tk/lib/components/misc/webpack/src/w/cfg/tabs.lhs.json" show={0}></MTKtabs>
+		    <MTKtabs id="my-tabs" cfg="http://links.melify.com/tk/lib/components/misc/webpack/src/w/cfg/remote/tabs.lhs.json" show={0}></MTKtabs>
 		</div>
 	    </div>
 	</div>
