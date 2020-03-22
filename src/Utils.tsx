2ca855aeb1af1df73018027763fe13e2299ddcbd
Mel <melheravi@gmail.com>
2020-03-22 19:20:02 -0400

    committed

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,7 +1,7 @@
 export const publish = (values: values): void => {
-
     console.log(values, "VALUES");
     var event = document.createEvent('CustomEvent');
+
     // @ts-ignore
     values.date = new Date().getTime();
     event.initCustomEvent(values.requestor, true, true, values);
@@ -11,7 +11,8 @@ export const publish = (values: values): void => {
 interface values {
     requestor: string
     request: "open" | "load"
-    panel: number
+    tab?: number
+    panel?: number
     url?: string
     html?: JSX.Element | string | JSX.Element[]
 }
--- src/components/tabs/MTKtabs.stories.tsx
+++ src/components/tabs/MTKtabs.stories.tsx
@@ -12,8 +12,9 @@ export const basic = () => {
                     <div className="col-md-12 margin-0 padding-0">
                         <div id="my-btn-group" className="btn-group" role="group" aria-label="Basic example">
                             <button type="button" className="btn btn-outline-secondary" onClick={() => publish({
-				requestor:"my-tabs", request:"open", tab:1}
-			    )} > 1 - Open Tab One</button>
+				requestor:"my-tabs", request:"open", tab:1
+			    })} > 1 - Open Tab One
+			    </button>
 
                             <button type="button" className="btn btn-outline-secondary" onClick={() => publish({
 				requestor:"my-tabs", request:"load", tab:1, url:"http://links.melify.com/mtk/render?ajax=1&callback=tk::dummy::3"
@@ -21,11 +22,12 @@ export const basic = () => {
                             </button>
 
                             <button type="button" className="btn btn-outline-secondary" onClick={() => publish({
-                                requestor:"my-tabs", request:"load", tab:1, html:"<h1>It worked!</h1>"})} > 3 - Load Tab One From URL
+                                requestor:"my-tabs", request:"load", tab:1, html:"<h1>It worked!</h1>"
+			    })} > 3 - Load Tab One From URL
                             </button>
                         </div>
-                    </div>
-                </div>
+		    </div>
+		</div>
             </div>
 
 	    <div className="container mt-5">
